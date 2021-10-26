8d7e132af5e932e20d142fb7c5b3a3ebfa80f098
LAPTOP-MADMF8IO\ASUS <dewafira@gmail.com>
2021-10-26 12:50:55 +0700

    selamat pagi

---
+++ aryarasendriya-60900119048-b/resources/views/biodata.blade.php
@@ -0,0 +1,39 @@
+<html>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tabel Biodata Diri dengan HTML</title>
+</head>
+
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+    <table border="1" cellspacing="0" cellpadding="5" align="center" width="800">
+        <tr align="center" bgcolor=white>
+            <td width="200">DATA DIRI</td>
+            <td width="300">KETERANGAN</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>arya rasendriya barata</td>
+        </tr>
+        <tr>
+            <td>kelas</td>
+            <td>b</td>
+        </tr>
+        <tr>
+            <td>nim</td>
+            <td>60900119048</td>
+        </tr>
+        <tr>
+            <td>email uin</td>
+            <td>60900119048@uin-alauddin.ac.id</td>
+        </tr>
+        
+    </table>
+</body>
+
+</html>
+
+</html>
+';
